b6b84efc7bfcbb3fe0626d71df57670247c3f93f
shulabh234 <shulabhdixit234@gmail.com>
2024-01-30 16:44:23 +0530

    all checks completed

--- util.py
+++ util.py
@@ -1,13 +1,13 @@
-# import os
-# import requests
-# from dotenv import load_dotenv
-# from typing import Final
+import os
+import requests
+from dotenv import load_dotenv
+from typing import Final
 
-# load_dotenv()
-# TOKEN: Final = os.getenv("TOKEN")
-# CHAT_ID: Final = os.getenv("CHAT_ID")
+load_dotenv()
+TOKEN: Final = os.getenv("TOKEN")
+CHAT_ID: Final = os.getenv("CHAT_ID")
 
 
-# def chatbot(message: str):
-#     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
-#     requests.get(url)
+def chatbot(message: str):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+    requests.get(url)
