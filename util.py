d2c6090394d9bfb698fd4782bc1719f2edfb0db9
shulabh234 <shulabhdixit234@gmail.com>
2024-01-30 16:27:02 +0530

    remove redudent code

--- util.py
+++ util.py
@@ -7,18 +7,7 @@ load_dotenv()
 TOKEN: Final = os.getenv("TOKEN")
 CHAT_ID: Final = os.getenv("CHAT_ID")
 
-"""TO GET THE CHAT ID"""
-# url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-# print(requests.get(url).json())
-
 
 def chatbot(message: str):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
     requests.get(url)
-
-
-# message = "hello python telegram bot"
-# url = (
-#     f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
-# )
-# print(requests.get(url).json())  # this sends the message
